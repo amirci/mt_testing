4886fe1e3b953a210e59ecbd9b3f4afc85f92155
David Alpert <david@spinthemoose.com>
2011-01-07 22:14:01 -0600

    added a test project (copied from MavenThought.Commons.Testing.NUnit.Tests and added the xunit pair to the sln.

---
+++ test/MavenThought.Commons.Testing.Xunit/SpecWithNoContract/CalculatorSpecification.cs
@@ -0,0 +1,11 @@
+using MavenThought.Commons.Testing.Example;
+
+namespace MavenThought.Commons.Testing.NUnit.Tests.SpecWithNoContract
+{
+    /// <summary>
+    /// Base class for calculator tests
+    /// </summary>
+    public class CalculatorSpecification : AutoMockSpecificationWithNoContract<Calculator>
+    {
+    }
+}
