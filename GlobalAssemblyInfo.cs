c2baac14f8c7a02ac7b2db25bce429d20bd04caf
Amir Barylko <amir@barylko.com>
2011-09-12 18:47:55 -0500

    Added missing nuget packages

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("MavenThought Inc.")]
-[assembly: AssemblyProduct("MavenThought Testing (sha 9afa6385014)")]
+[assembly: AssemblyProduct("MavenThought Testing (sha 0de1568292d)")]
 [assembly: AssemblyCopyright("MavenThought Inc. 2006 - 2011")]
 [assembly: AssemblyVersion("0.3.6")]
 
